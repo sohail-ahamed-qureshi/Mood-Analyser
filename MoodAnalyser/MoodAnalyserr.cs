c4b1af0ee6effff3e4011d3b7e3e0b3119da5232
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-17 23:55:45 +0530

    [sohail] Add: Ability to respond Happy or sad mood

---
+++ MoodAnalyser/MoodAnalyserr.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserr
+    {
+        //constants
+        private const string SAD_MESSAGE = "Iam in Sad Mood";
+        private const string HAPPY_MESSAGE = "Iam in Any Mood";
+        /// <summary>
+        /// Check for mood if sad return sad message and
+        /// if happy return happy message.
+        /// </summary>
+        /// <param name="message">string passed by user</param>
+        /// <returns></returns>
+        public string MoodCheck(string message)
+        {
+            if (message == SAD_MESSAGE)
+                return "SAD";
+            if (message == HAPPY_MESSAGE)
+                return "HAPPY";
+            return null;
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        /// <summary>
+        /// Mood Analyser Program using Exceptions.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser program using Exceptions");
+
+            MoodAnalyserr moodAnalyserr = new MoodAnalyserr();
+            Console.WriteLine(moodAnalyserr.MoodCheck("Iam in Sad Mood"));
+        }
+    }
+}
